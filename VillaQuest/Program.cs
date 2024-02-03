1ae7efc0426f47c992fed2df90cc98e70c01294b
Swapnil <joshiswapnil2019@gmail.com>
2024-02-04 00:02:59 +0530

    Added Project Structure,Db Connection,Defined Villa Database table and Create Db table.

--- VillaQuest/Program.cs
+++ VillaQuest/Program.cs
@@ -1,8 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using VillaQuest.Infrastrucutre.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<ApplicationDBContext>(option=> option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
