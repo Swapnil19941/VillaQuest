0a9635b8c376e10baa47d714b33e95e3964bdca1
Swapnil <joshiswapnil2019@gmail.com>
2024-02-05 23:06:19 +0530

    Added Villa Index Controller and Fetched Villas from Database and shown to UI

---
+++ VillaQuest/Controllers/VillaController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VillaQuest.Infrastrucutre.Data;
+
+namespace VillaQuest.Controllers
+{
+    public class VillaController : Controller
+    {
+        private ApplicationDBContext _dbContext;
+
+        public VillaController(ApplicationDBContext dBContext)
+        {
+            _dbContext = dBContext;
+        }
+        public IActionResult Index()
+        {
+            var villas = _dbContext.Villas.ToList();    
+           return View(villas);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
